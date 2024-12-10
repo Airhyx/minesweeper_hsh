6a46639a45802aba55420bc45bd835cdc3ae5d6c
Alexander Scott McGuigan <47324365+Airhyx@users.noreply.github.com>
2024-12-10 09:33:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,53 +8,63 @@
 #define SIZE 10
 #define FLAG_COLOR 2
 
-void cleanup_ncurses(WINDOW *grid_win) {
+void cleanupNcurses(WINDOW *grid_win) 
+{
     delwin(grid_win);
     endwin();
 }
 
-int main() {
+int main() 
+{
     char gridFront[SIZE][SIZE];
     char gridBack[SIZE][SIZE];
-    bool flagged[SIZE][SIZE] = {false}; // Track flagged cells
-    bool visited[SIZE][SIZE] = {false}; //
+    bool flagged[SIZE][SIZE] = {false}; 
+    bool visited[SIZE][SIZE] = {false}; 
 
     createGrid(gridFront);
-    setBombs(gridBack, TOTAL_BOMBS); // Set bombs and numbers
+    setBombs(gridBack, TOTAL_BOMBS); 
     checkNumbers(gridBack);
 
-    int cursor_row = SIZE / 2, cursor_col = SIZE / 2; // Cursor starts in the middle
+    int cursor_row = SIZE / 2, cursor_col = SIZE / 2; // Cursor startet mittig
 
-    initialize_ncurses();
+    initializeNcurses();
 
-    WINDOW *grid_win = create_grid_window(SIZE, SIZE);
-    draw_grid(grid_win, SIZE, SIZE, gridFront, gridBack, cursor_row, cursor_col, flagged);
+    WINDOW *grid_win = createGridWindow(SIZE, SIZE);
+    drawGrid(grid_win, SIZE, SIZE, gridFront, gridBack, cursor_row, cursor_col, flagged);
 
-    while (1) {
-        draw_grid(grid_win, SIZE, SIZE, gridFront, gridBack, cursor_row, cursor_col, flagged); // Redraw the grid
+    while (1) 
+    {
+        drawGrid(grid_win, SIZE, SIZE, gridFront, gridBack, cursor_row, cursor_col, flagged); // Grid neu drucken
 
-        int move = input(); // Get the user input
+        int move = input(); 
 
-        if (move == 1 || move == 2 || move == 3 || move == 4) { // Movement keys
-            handle_movement(&cursor_row, &cursor_col, SIZE, SIZE, move);
-        } else if (move == 5) { // Flag the current cell
-            if (gridFront[cursor_row][cursor_col] == '#') {
-                flagged[cursor_row][cursor_col] = !flagged[cursor_row][cursor_col];
+        if (move == 1 || move == 2 || move == 3 || move == 4) // Input WASD oder Pfeiltasten
+        {
+            handleMovement(&cursor_row, &cursor_col, SIZE, SIZE, move);
+        } else if (move == 5) 
+        { 
+            if (gridFront[cursor_row][cursor_col] == '#') 
+            {
+                flagged[cursor_row][cursor_col] = !flagged[cursor_row][cursor_col]; // Toggle Flag
             }
-        } else if (move == 6) { // Clear the current cell
-            if (flagged[cursor_row][cursor_col]) {
-                continue; // Do not clear flagged cells
+        } else if (move == 6) 
+        { 
+            if (flagged[cursor_row][cursor_col]) 
+            {
+                continue; // Zellen mit Flagge nicht clearen
             }
-            if (gridBack[cursor_row][cursor_col] == 'x') {
+            if (gridBack[cursor_row][cursor_col] == 'x') 
+            {
                 mvprintw(SIZE + 2, 0, "You hit a bomb! Game over!");
                 refresh();
-                break; // Game over
-            } else {
-                clear_cell(gridFront, gridBack, visited, cursor_row, cursor_col); // Reveal the cell
+                break; // Game over, falls Bombe bei Clear
+            } else 
+            {
+                clearCell(gridFront, gridBack, visited, cursor_row, cursor_col);
             }
         }
     }
 
-    cleanup_ncurses(grid_win);
+    cleanupNcurses(grid_win);
     return 0;
 }
